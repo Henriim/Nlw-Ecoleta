429bb2174a573cc1987b3f1ed447685d206c6f2a
Henrique Mirenda <drummerhenrique@hotmail.com>
2020-06-02 12:01:06 -0300

    Criação do banco de dados utilizando Knex e Sqlite

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,17 +1,7 @@
 import express from 'express';
+import routes from './routes';
 
 const app = express();
-
-app.get('/users',(request, response) =>{
-  console.log("listagem de usuarios");
-
-  response.json([
-    'Henrique',
-    'Diego',
-    'Daniel',
-    'cleiton',
-    'Miguel'
-  ]);
-});
-
+app.use(express.json());
+app.use(routes);
 app.listen(3333);
